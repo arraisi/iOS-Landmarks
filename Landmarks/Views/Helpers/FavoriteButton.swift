b796d4889d48d6efe8d5105cf19182f7e579a919
abdul <abdul.rahman@geekseat.com.au>
2021-12-02 12:56:48 +0700

    handling user input

---
+++ Landmarks/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,28 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Abdul R. Arraisi on 02/12/21.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toogle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
---
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,49 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Abdul R. Arraisi on 02/12/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoriteOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoriteOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoriteOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+                .navigationTitle("Landmarks")
+            }
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (2nd generation)", "iPhone 13"], id: \.self) { device in
+            LandmarkList()
+                .environmentObject(ModelData())
+                .previewDevice(PreviewDevice(rawValue: device))
+                .previewDisplayName(device)
+        }
+    }
+}
